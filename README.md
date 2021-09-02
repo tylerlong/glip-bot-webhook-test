29e59002d0b3fc8503f52c39b79e4bae7bde1b1a
Tyler Long <tyler4long@gmail.com>
2021-09-02 13:36:57 -0700

    First working version

---
+++ README.md
@@ -0,0 +1,8 @@
+# glip-bot-webhook-test
+
+Rename `.env.sample` to `.env` and specify credentials.
+
+```
+yarn install
+yarn test
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello world');
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  server: 'https://platform.devtest.ringcentral.com',
+});
+
+rc.token = {access_token: process.env.RINGCENTRAL_TOKEN};
+
+(async () => {
+  const subInfo = await rc
+    .restapi()
+    .subscription()
+    .post({
+      eventFilters: ['/restapi/v1.0/glip/posts'],
+      deliveryMode: {
+        address: process.env.WEBHOOK_ADDRESS,
+        transportType: 'WebHook',
+      },
+    });
+  console.log(JSON.stringify(subInfo, null, 2));
+
+  const subList = await rc.restapi().subscription().list();
+  console.log(JSON.stringify(subList, null, 2));
+})();
